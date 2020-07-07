a9a3442b83bfa640781f3cf7c87c5dc464ff8541
Obaidullah Zia Khan <obaidullahzia.khan@student.griffith.ie>
2020-07-07 20:22:00 +0100

    components react 1

--- React/mern_react_1/src/App.js
+++ React/mern_react_1/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';  // frist reactstrap componet we called.
+import Menu from './components/MenuComponent';      // component for menu list taht we made.
 import './App.css';
 
 
@@ -10,13 +11,25 @@ import './App.css';
 class App extends Component {     
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/"> Navbar brand </NavbarBrand>
           </div>
         </Navbar>
-  
+        <Menu />
+
+        {/*
+          Now, my menu component will be rendered below the nav bar in my app component. 
+          So now, you can see how I have subdivided my UI into two parts. 
+          There is this one menu component. And then on top,
+          you have the Navbar component, which is enclosed inside the app component, 
+          which is then rendered by the index.js file here as a single component. 
+          So you can see how one component can be imported into another component 
+          and then made use of in the other component in order to construct 
+          the overall user interface. 
+        */}
+ 
       </div>
     );
   }
