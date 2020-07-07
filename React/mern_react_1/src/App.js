3fab832916834a4b7dddeb9487a3d07893f01a17
Obaidullah Zia Khan <obaidullahzia.khan@student.griffith.ie>
2020-07-07 18:13:37 +0100

    reactStrap for navbar and navbarBrand

--- React/mern_react_1/src/App.js
+++ React/mern_react_1/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';  // frist reactstrap componet we called.
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Components are independent and reusable bits of code. 
+//They serve the same purpose as JavaScript functions, 
+//but work in isolation and returns HTML via a render function.
+class App extends Component {     
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/"> Navbar brand </NavbarBrand>
+          </div>
+        </Navbar>
+  
+      </div>
+    );
+  }
+
 }
 
 export default App;
